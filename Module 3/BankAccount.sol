fcb55b74cfaf11bfa2a53095b9ca94c27b6ebfa1
Joshua Peterson <jpeterson2058@gmail.com>
2022-12-29 15:44:23 -0700

    Update BankAccount.sol

--- Module 3/BankAccount.sol
+++ Module 3/BankAccount.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.5.0;
 
 contract BankAccount {
+    address payable accountOwner = 0x838B260B95e04ff23D5066B9af0211E683539F2F;
+
     function withdraw(uint amount, address payable recipient) public {
-        return recipient.transfer(amount);
+        require(recipient == accountOwner, "You don't own this account homeslice!");
+            return recipient.transfer(amount);   
     }
 
     function deposit() public payable{}
