6191cd120b676beb4d0d178ee6a13c9bd31e3262
Joshua Peterson <jpeterson2058@gmail.com>
2022-12-29 15:51:11 -0700

    Update CustomerAccount.sol

--- Module 3/CustomerAccount.sol
+++ Module 3/CustomerAccount.sol
@@ -1,19 +1,21 @@
 pragma solidity ^0.5.0;
 
 contract CustomerAccount {
-    address owner;
+    address payable owner;
     bool isNewAccount;
     uint public accountBalance;
     string customerName;
     string customerLastName;
+    address payable authorizedRecipient;
 
     function getInfo() view public returns(address, bool, uint, string memory, string memory) {
         return(owner, isNewAccount, accountBalance, customerName, customerLastName);
 
     }
 
-    function setInfo(address newOwner, bool newAccountStatus, uint newAccountBalance, string memory newCustomerName, string memory newCustomerLastName) public {
+    function setInfo(address payable newOwner, address payable newAuthorizedRecipient, bool newAccountStatus, uint newAccountBalance, string memory newCustomerName, string memory newCustomerLastName) public {
         owner = newOwner;
+        authorizedRecipient = newAuthorizedRecipient;
         isNewAccount = newAccountStatus;
         accountBalance = newAccountBalance;
         customerName = newCustomerName;
@@ -22,6 +24,7 @@ contract CustomerAccount {
     }
 
     function sendRemittance(uint amount, address payable recipient) public {
+        require(recipient == owner || recipient == authorizedRecipient, "You don't own this account homeslice!");
         recipient.transfer(amount);
         accountBalance = address(this).balance;
     }
