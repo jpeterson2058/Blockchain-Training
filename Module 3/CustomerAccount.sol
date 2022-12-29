1cae34921a58cce840dbce329b3b31c0052cff97
Joshua Peterson <jpeterson2058@gmail.com>
2022-12-29 15:22:02 -0700

    Update CustomerAccount.sol

--- Module 3/CustomerAccount.sol
+++ Module 3/CustomerAccount.sol
@@ -6,4 +6,18 @@ contract CustomerAccount {
     uint accountBalance = 50000;
     string customerName = "Buddy";
     string customerLastName = "Elf";
+
+    function getInfo() view public returns(address, bool, uint, string memory, string memory) {
+        return(owner, isNewAccount, accountBalance, customerName, customerLastName);
+
+    }
+
+    function setInfo(address newOwner, bool newAccountStatus, uint newAccountBalance, string memory newCustomerName, string memory newCustomerLastName) public {
+        owner = newOwner;
+        isNewAccount = newAccountStatus;
+        accountBalance = newAccountBalance;
+        customerName = newCustomerName;
+        customerLastName = newCustomerLastName;
+
+    }
 }
