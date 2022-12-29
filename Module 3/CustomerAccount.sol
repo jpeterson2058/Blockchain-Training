5dfa948839655c1e1bee9d0b9bb7d1c61cde4a93
Joshua Peterson <jpeterson2058@gmail.com>
2022-12-29 11:07:09 -0700

    Create CustomerAccount.sol

---
+++ Module 3/CustomerAccount.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract CustomerAccount {
+    address owner = 0xc3879B456DAA348a16B6524CBC558d2CC984722c;
+    bool isNewAccount = true;
+    uint accountBalance = 50000;
+    string customerName = "Buddy";
+    string customerLastName = "Elf";
+}
